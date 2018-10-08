caa1d61513ce484919a9338b80cb87aa57cce7a9
Susheelkumar44 <susheelkumar.3615@gmail.com>
2018-10-08 09:06:43 +0530

    Update DateDifferenceTest.java

--- DateDifference/src/main/java/com/susheel/test/DateDifferenceTest.java
+++ DateDifference/src/main/java/com/susheel/test/DateDifferenceTest.java
@@ -1,76 +1,129 @@
-package com.susheel.test;
+package com.susheel.date.test;
+import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
+import org.junit.Test;
 
 import com.susheel.date.MyDate;
 import com.susheel.date.service.DateDifferenceProvider;
 
-public class DateDifferenceTest {
+public class TestCaseClassJunit {
+	@Test
+	public void testCaseForDateDifferences1() 
+	{
+		MyDate startDate = new MyDate(06, 04, 2011);
+		MyDate endDate = new MyDate(06, 04, 2011);
+		assertEquals(0, DateDifferenceProvider.getDateDifference(startDate, endDate));
+	}
+	
+	@Test
+	public void testCaseForDateDifferences2() 
+	{
+		MyDate startDate = new MyDate(06, 04, 2011);
+		MyDate endDate = new MyDate(18, 04, 2011);
+		assertEquals(12, DateDifferenceProvider.getDateDifference(startDate, endDate));
+	}
+	
+	@Test
+	public void testCaseForDateDifferences3() 
+	{
+		MyDate startDate = new MyDate(06, 04, 2011);
+		MyDate endDate = new MyDate(18, 05, 2011);
+		assertEquals(42, DateDifferenceProvider.getDateDifference(startDate, endDate));
+	}
+	
+	@Test
+	public void testCaseForDateDifferences4() 
+	{
+		MyDate startDate = new MyDate(06, 04, 2011);
+		MyDate endDate = new MyDate(18, 06, 2011);
+		assertEquals(73, DateDifferenceProvider.getDateDifference(startDate, endDate));
+	}
+	
+	@Test
+	public void testCaseForDateDifferences5() 
+	{
+		MyDate startDate = new MyDate(06, 04, 2011);
+		MyDate endDate = new MyDate(18, 12, 2011);
+		assertEquals(256, DateDifferenceProvider.getDateDifference(startDate, endDate));
+	}
+	
+	@Test
+	public void testCaseForDateDifferences6() 
+	{
+		MyDate startDate = new MyDate(06, 04, 2011);
+		MyDate endDate = new MyDate(18, 12, 2012);
+		assertEquals(622, DateDifferenceProvider.getDateDifference(startDate, endDate));
+	}
+	
+	@Test
+	public void testCaseForDateDifferences7() 
+	{
+		MyDate startDate = new MyDate(06, 04, 2011);
+		MyDate endDate = new MyDate(18, 12, 2013);
+		assertEquals(987, DateDifferenceProvider.getDateDifference(startDate, endDate));
+	}
+	
+	@Test
+	public void testCaseForDateDifferences8() 
+	{
+		MyDate startDate = new MyDate(06, 04, 2011);
+		MyDate endDate = new MyDate(18, 12, 2113);
+		assertEquals(37511, DateDifferenceProvider.getDateDifference(startDate, endDate));
+	}
+	
+	@Test
+	public void testCaseForDateDifferences9() 
+	{
+		MyDate startDate = new MyDate(06, 04, 2011);
+		MyDate endDate = new MyDate(18, 12, 2413);
+		assertEquals(147084, DateDifferenceProvider.getDateDifference(startDate, endDate));
+	}
+	
+	@Test
+	public void testCaseForDateDifferences10() 
+	{
+		MyDate startDate = new MyDate(06, 04, 2011);
+		MyDate endDate = new MyDate(18, 12, 2813);
+		assertEquals(293181, DateDifferenceProvider.getDateDifference(startDate, endDate));
+	}
+	
+	@Test
+	public void testCaseForDateDifferences11() 
+	{
+		MyDate startDate = new MyDate(06, 01, 2011);
+		MyDate endDate = new MyDate(06, 03, 2011);
+		assertEquals(59, DateDifferenceProvider.getDateDifference(startDate, endDate));
+	}
+	
+	@Test
+	public void testCaseForDateDifferences12() 
+	{
+		MyDate startDate = new MyDate(06, 01, 2012);
+		MyDate endDate = new MyDate(06, 03, 2012);
+		assertEquals(60, DateDifferenceProvider.getDateDifference(startDate, endDate));
+	}
+	
+	@Test
+	public void testCaseForDateDifferences13() 
+	{
+		MyDate startDate = new MyDate(06, 02, 2012);
+		MyDate endDate = new MyDate(06, 03, 2012);
+		assertEquals(29, DateDifferenceProvider.getDateDifference(startDate, endDate));
+	}
+	
+	@Test
+	public void testCaseForDateDifferences14() 
+	{
+		MyDate startDate = new MyDate(22, 01, 2012);
+		MyDate endDate = new MyDate(15, 11, 2012);
+		assertEquals(298, DateDifferenceProvider.getDateDifference(startDate, endDate));
+	}
 
-	public static void main(String[] args) {
-
-		ArrayList<MyDateTestRecord> testData = new ArrayList<>();
-
-		// case1
-		testData.add(new MyDateTestRecord(new MyDate(06, 04, 2011), new MyDate(06, 04, 2011), 0));
-
-		// case2
-		testData.add(new MyDateTestRecord(new MyDate(06, 04, 2011), new MyDate(18, 04, 2011), 12));
-
-		// case3
-		testData.add(new MyDateTestRecord(new MyDate(06, 04, 2011), new MyDate(18, 05, 2011), 42));
-
-		// case4
-		testData.add(new MyDateTestRecord(new MyDate(06, 04, 2011), new MyDate(18, 06, 2011), 73));
-
-		// case5
-		testData.add(new MyDateTestRecord(new MyDate(06, 04, 2011), new MyDate(18, 12, 2011), 256));
-
-		// case6
-		testData.add(new MyDateTestRecord(new MyDate(06, 04, 2011), new MyDate(18, 12, 2012), 622));
-
-		// case7
-		testData.add(new MyDateTestRecord(new MyDate(06, 04, 2011), new MyDate(18, 12, 2013), 987));
-
-		// case8
-		testData.add(new MyDateTestRecord(new MyDate(06, 04, 2011), new MyDate(18, 12, 2113), 37511));
-
-		// case9
-		testData.add(new MyDateTestRecord(new MyDate(06, 04, 2011), new MyDate(18, 12, 2413), 147084));
-
-		// case10
-		testData.add(new MyDateTestRecord(new MyDate(06, 04, 2011), new MyDate(18, 12, 2813), 293181));
-
-		// case11
-		testData.add(new MyDateTestRecord(new MyDate(06, 01, 2011), new MyDate(06, 03, 2011), 59));
-
-		// case12
-		testData.add(new MyDateTestRecord(new MyDate(06, 01, 2012), new MyDate(06, 03, 2012), 60));
-
-		// case13
-		testData.add(new MyDateTestRecord(new MyDate(06, 02, 2012), new MyDate(06, 03, 2012), 29));
-
-		// case14
-		testData.add(new MyDateTestRecord(new MyDate(22, 01, 2012), new MyDate(15, 11, 2012), 298));
-
-		// case15
-		testData.add(new MyDateTestRecord(new MyDate(06, 02, 2012), new MyDate(06, 12, 2012), 304));
-
-		for (MyDateTestRecord testCase : testData) {
-
-			MyDate startDate = testCase.startDate;
-			MyDate endDate = testCase.endDate;
-			long expectedResult = testCase.expectedResult;
-			long obtainedResult = DateDifferenceProvider.getDateDifference(startDate, endDate);
-
-			if (expectedResult == obtainedResult)
-				System.out.println("Test " + (1 + testData.lastIndexOf(testCase)) + " Passed " + obtainedResult
-						+ " = obtainedResult " + expectedResult + " = expectedResult");
-			else
-				System.err.println("Test " + (1 + testData.lastIndexOf(testCase)) + " Failed " + obtainedResult
-						+ " = obtainedResult " + expectedResult + " = expectedResult");
-
-		}
-
+	@Test
+	public void testCaseForDateDifferences15() 
+	{
+		MyDate startDate = new MyDate(06, 02, 2012);
+		MyDate endDate = new MyDate(06, 12, 2012);
+		assertEquals(304, DateDifferenceProvider.getDateDifference(startDate, endDate));
 	}
 }
